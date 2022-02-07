0b42bcb900b9b566f739c8711122fcfba246622f
LnxGuillermo <guile007@gmail.com>
2022-02-07 11:05:06 -0500

    agregando codacy

--- my_script.sh
+++ my_script.sh
@@ -1,1 +1,5 @@
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+#Bash for codecov
+#bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+
+#Bash for codacy
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r target/test-results/jacoco/*.xml
